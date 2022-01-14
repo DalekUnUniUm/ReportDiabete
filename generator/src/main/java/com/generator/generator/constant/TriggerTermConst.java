43525851616837fb3dd55698735fd042745c16c3
DalekUnUniUm <martin.romuald2@hotmail.fr>
2022-01-14 22:06:07 +0100

    V1.0.2

--- generator/src/main/java/com/generator/generator/constant/TriggerTermConst.java
+++ generator/src/main/java/com/generator/generator/constant/TriggerTermConst.java
@@ -1,17 +1,38 @@
 package com.generator.generator.constant;
 
-public class TriggerTermConst {
+public class TriggerTermConst{
 
-    public static final String[] TRIGGER_TERM_LIST = {"Hemoglobin A1C",
-                                                "Microalbumin",
-                                                "Height",
-                                                "Weight",
-                                                "Smoker",
-                                                "Abnormal",
-                                                "Cholesterol",
-                                                "Dizziness",
-                                                "Relapse",
-                                                "Reaction",
-                                                "Antibody"};
+    public static final String[] TRIGGER_TERM_LIST = TriggerTermConstTest.TRIGGER_TERM_LIST ;
 
+    //public static final String[] TRIGGER_TERM_LIST = TriggerTermConstRelease.TRIGGER_TERM_LIST ;
+
+}
+class TriggerTermConstTest {
+    public static final String[] TRIGGER_TERM_LIST = {
+            "Hemoglobin A1C",
+            "Microalbumin",
+            "Height",
+            "Weight",
+            "Smoker",
+            "Abnormal",
+            "Cholesterol",
+            "Dizziness",
+            "Relapse",
+            "Reaction",
+            "Antibody"};
+
+}
+class TriggerTermConstRelease{
+    public static final String[] TRIGGER_TERM_LIST = {
+            "Hémoglobine A1C",
+            "Microalbumine",
+            "Taille",
+            "Poids",
+            "Fumeur",
+            "Anormal",
+            "Cholestérol",
+            "Vertige",
+            "Rechute",
+            "Réaction",
+            "Anticorps"};
 }
